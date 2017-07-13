1e385c87df9ebe50845d9acdee9163cff506e3e3
mrmagooey <mrmagooey@users.noreply.github.com>
2017-07-13 14:48:21 +1000

    change api route code-configuration to code-parameters

--- routes.go
+++ routes.go
@@ -7,7 +7,7 @@ func register_routes() *mux.Router {
 	r := mux.NewRouter()
 	v1_subroute := r.PathPrefix("/v1").Subrouter()
 	v1_subroute.Schemes("https")
-	v1_subroute.Methods("POST").Path("/code-configuration/").HandlerFunc(apiV1.SetCodeConfig())
+	v1_subroute.Methods("POST").Path("/code-parameters/").HandlerFunc(apiV1.SetCodeConfig())
 	v1_subroute.Methods("POST").Path("/code-name/").HandlerFunc(apiV1.SetCodeName())
 	v1_subroute.Methods("POST").Path("/command/").HandlerFunc(apiV1.Command())
 	return r
