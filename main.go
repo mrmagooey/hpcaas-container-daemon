693ba6275fc645a22ecd34aa7ade269bf6c98b5d
mrmagooey <mrmagooey@users.noreply.github.com>
2017-08-17 16:03:32 +1000

    test tls setup

--- main.go
+++ main.go
@@ -2,60 +2,54 @@ package main
 
 import (
 	"crypto/tls"
-	"crypto/x509"
 	"github.com/mrmagooey/hpcaas-container-daemon/state"
 	"io/ioutil"
-	"log"
 	"net/http"
 	"os"
 )
 
-var STARTUP_FILE = "/hpcaas/daemon/daemon_has_started"
-var TLS_CLIENT_CRT = "/hpcaas/daemon/tls_client.crt"
-var TLS_CERT_FILE = "/hpcaas/daemon/tls_server.crt"
-var TLS_KEY_FILE = "/hpcaas/daemon/tls_server.key"
-var AUTHORIZATION = "/hpcaas/daemon/authorization"
+var startupFile = "/hpcaas/daemon/daemon_has_started"
+var tlsCertFile = "/hpcaas/daemon/tls_server.crt"
+var tlsKeyFile = "/hpcaas/daemon/tls_server.key"
+
+var daemonPublicCertEnvVar = "TLS_PUBLIC_CERT"
+var daemonPrivateKeyEnvVar = "TLS_PRIVATE_KEY"
+var daemonAuthEnvVar = "AUTHORIZATION"
 
 // run once at container startup
 // pull comm information out of environment variables and save to disk
 func setupTLSInfo() {
-	tls_public_cert, envErr := os.LookupEnv("TLS_PUBLIC_CERT")
+	tlsPublicCert, envErr := os.LookupEnv(daemonPublicCertEnvVar)
 	if envErr == false {
 		panic("TLS certificate is missing from environment variables")
 	}
-	tls_private_key, envErr := os.LookupEnv("TLS_PRIVATE_KEY")
+	tlsPrivateKey, envErr := os.LookupEnv(daemonPrivateKeyEnvVar)
 	if envErr == false {
 		panic("TLS key is missing from environment variables")
 	}
-	auth_key, envErr := os.LookupEnv("AUTHORIZATION")
+	authKey, envErr := os.LookupEnv(daemonAuthEnvVar)
 	if envErr == false {
 		panic("authorization is missing from environment variables")
 	}
-	tls_client_crt, envErr := os.LookupEnv("TLS_CLIENT_CRT")
-	if envErr == false {
-		panic("TLS certificate is missing from environment variables")
-	}
-	err := ioutil.WriteFile(TLS_CLIENT_CRT, []byte(tls_client_crt), 0300)
+	//
+	err := ioutil.WriteFile(tlsCertFile, []byte(tlsPublicCert), 0300)
 	if err != nil {
 		panic("Couldn't save tls server certificate to disk")
 	}
-	err = ioutil.WriteFile(TLS_CERT_FILE, []byte(tls_public_cert), 0300)
-	if err != nil {
-		panic("Couldn't save tls server certificate to disk")
-	}
-	err = ioutil.WriteFile(TLS_KEY_FILE, []byte(tls_private_key), 0300)
+	err = ioutil.WriteFile(tlsKeyFile, []byte(tlsPrivateKey), 0300)
 	if err != nil {
 		panic("Couldn't save tls server key to disk")
 	}
-	state.SetAuthorizationKey(auth_key)
+
+	state.SetAuthorizationKey(authKey)
 }
 
 // check if this is the first time that the daemon has started up
 // if the daemon was previously running
 func daemonStartup() {
-	if _, err := os.Stat(STARTUP_FILE); err != nil {
+	if _, err := os.Stat(startupFile); err != nil {
 		// startup file doesn't exist, this is the first time the daemon has started
-		f, err := os.Create(STARTUP_FILE)
+		f, err := os.Create(startupFile)
 		if err != nil {
 			panic("Couldn't write startup file")
 		}
@@ -71,18 +65,16 @@ func daemonStartup() {
 
 // setup the tls information for the server
 func setupServer() *http.Server {
-	clientCert, err := ioutil.ReadFile(TLS_CLIENT_CRT)
-	if err != nil {
-		log.Fatal(err)
-	}
-	clientCertPool := x509.NewCertPool()
-	clientCertPool.AppendCertsFromPEM(clientCert)
-	// Setup HTTPS client
 	tlsConfig := &tls.Config{
-		ClientCAs:  clientCertPool,
-		ClientAuth: tls.RequireAndVerifyClientCert,
-	}
-	tlsConfig.BuildNameToCertificate()
+		MinVersion:               tls.VersionTLS12,
+		CurvePreferences:         []tls.CurveID{tls.CurveP521, tls.CurveP384, tls.CurveP256},
+		PreferServerCipherSuites: true,
+		CipherSuites: []uint16{
+			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
+			tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_RSA_WITH_AES_256_CBC_SHA,
+		}}
 	routes := registerRoutes()
 	server := &http.Server{
 		Addr:      ":443",
@@ -92,9 +84,10 @@ func setupServer() *http.Server {
 	return server
 }
 
+//
 func main() {
 	daemonStartup()
 	setupTLSInfo()
 	server := setupServer()
-	server.ListenAndServeTLS(TLS_CERT_FILE, TLS_KEY_FILE)
+	server.ListenAndServeTLS(tlsCertFile, tlsKeyFile)
 }
--- main_test.go
+++ main_test.go
@@ -1,15 +1,175 @@
 package main
 
-import "testing"
-import "os"
-import "github.com/stretchr/testify/assert"
+import (
+	"bytes"
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"encoding/pem"
+	"errors"
+	"fmt"
+	"github.com/mrmagooey/hpcaas-container-daemon/state"
+	"github.com/stretchr/testify/assert"
+	"io/ioutil"
+	"math/big"
+	"os"
+	"testing"
+	"time"
+)
 
-var TEST_ENV_VAR = "HPCAAS_DAEMON_TEST_CONTAINER"
+// https://golang.org/src/crypto/tls/generate_cert.go
+func generateCACertAndKey() (certBytes []byte, keyBytes []byte, err error) {
+	caPrivateKey, err := rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		return nil, nil, err
+	}
+	notBefore := time.Now()
+	notAfter := notBefore.AddDate(10, 0, 0)
+	serialNumberLimit := new(big.Int).Lsh(big.NewInt(1), 128)
+	serialNumber, err := rand.Int(rand.Reader, serialNumberLimit)
+	if err != nil {
+		return nil, nil, err
+	}
+	caTemplate := x509.Certificate{
+		SerialNumber: serialNumber,
+		Subject: pkix.Name{
+			Organization: []string{"HPCAAS"},
+		},
+		NotBefore:             notBefore,
+		NotAfter:              notAfter,
+		KeyUsage:              x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature,
+		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth},
+		BasicConstraintsValid: true,
+	}
+	// caTemplate.DNSNames = append(caTemplate.DNSNames, "no-domain-here.example.com")
+	caTemplate.IsCA = true
+	caCertBytes, err := x509.CreateCertificate(rand.Reader, &caTemplate, &caTemplate, &caPrivateKey.PublicKey, caPrivateKey)
+	if err != nil {
+		return nil, nil, err
+	}
+	caPemBlock := &pem.Block{Type: "CERTIFICATE", Bytes: caCertBytes}
+	privPemBlock := &pem.Block{Type: "RSA PRIVATE KEYS", Bytes: x509.MarshalPKCS1PrivateKey(caPrivateKey)}
+	certBuf := &bytes.Buffer{}
+	keyBuf := &bytes.Buffer{}
+	err = pem.Encode(certBuf, caPemBlock)
+	if err != nil {
+		return nil, nil, err
+	}
 
-func TestServer(t *testing.T) {
-	assert.Equal(t, 1, 1)
+	err = pem.Encode(keyBuf, privPemBlock)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return certBuf.Bytes(), keyBuf.Bytes(), nil
 }
 
-func TestMain(m *testing.M) {
-	os.Exit(m.Run())
+func generateNewCert(caCertBytes []byte, caKeyBytes []byte) ([]byte, []byte, error) {
+	// regenerate ca info from byte buffers, pretending that we've loaded files
+	caCertPemBlock, _ := pem.Decode(caCertBytes)
+	if caCertPemBlock == nil {
+		return nil, nil, errors.New("failed to decode caCertBytes")
+	}
+
+	caCert, err := x509.ParseCertificate(caCertPemBlock.Bytes)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	caKeyPemBlock, _ := pem.Decode(caKeyBytes)
+	if caKeyPemBlock == nil {
+		return nil, nil, errors.New("failed to decode caKeyBytes ")
+	}
+
+	caKey, err := x509.ParsePKCS1PrivateKey(caKeyPemBlock.Bytes)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	// generate new cert
+	privateKey, err := rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	notBefore := time.Now()
+	notAfter := notBefore.AddDate(10, 0, 0)
+	serialNumberLimit := new(big.Int).Lsh(big.NewInt(1), 128)
+	serialNumber, _ := rand.Int(rand.Reader, serialNumberLimit)
+	template := x509.Certificate{
+		SerialNumber: serialNumber,
+		Subject: pkix.Name{
+			Organization: []string{"HPCAAS"},
+		},
+		NotBefore:             notBefore,
+		NotAfter:              notAfter,
+		KeyUsage:              x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature,
+		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth},
+		BasicConstraintsValid: true,
+	}
+	template.DNSNames = append(template.DNSNames, "no-domain-here.example.com")
+	template.IsCA = false
+
+	certBytes, err := x509.CreateCertificate(rand.Reader, &template, caCert, &privateKey.PublicKey, caKey)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	pemBlock := &pem.Block{Type: "CERTIFICATE", Bytes: certBytes}
+	privPemBlock := &pem.Block{Type: "RSA PRIVATE KEYS", Bytes: x509.MarshalPKCS1PrivateKey(privateKey)}
+
+	certBuf := &bytes.Buffer{}
+	keyBuf := &bytes.Buffer{}
+
+	pem.Encode(certBuf, pemBlock)
+	pem.Encode(keyBuf, privPemBlock)
+
+	return certBuf.Bytes(), keyBuf.Bytes(), nil
+}
+
+// generate the better part of a x509 cert signing chain
+// test that the daemon can pick up a correctly signed x509 certificate
+// from the environment variables
+func TestTLSInformation(t *testing.T) {
+	assert := assert.New(t)
+	state.InitState()
+	// generate tls information
+	caCertBytes, caKeyBytes, err := generateCACertAndKey()
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	daemonCertBytes, daemonKeyBytes, err := generateNewCert(caCertBytes, caKeyBytes)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	// generate auth key
+	numberLimit := new(big.Int).Lsh(big.NewInt(1), 128)
+	authInt, _ := rand.Int(rand.Reader, numberLimit)
+	authKey := fmt.Sprintf("%x", authInt)
+	// set the env variables
+	os.Setenv(daemonPublicCertEnvVar, string(daemonCertBytes))
+	os.Setenv(daemonPrivateKeyEnvVar, string(daemonKeyBytes))
+	os.Setenv(daemonAuthEnvVar, authKey)
+	// setup tls info
+	setupTLSInfo()
+	// check that the files have been correctly written to where they need to be
+	loadedCert, err := ioutil.ReadFile(tlsCertFile)
+	assert.NoError(err)
+	loadedKey, err := ioutil.ReadFile(tlsKeyFile)
+	assert.NoError(err)
+
+	certPemBlock, _ := pem.Decode(loadedCert)
+	_, err = x509.ParseCertificate(certPemBlock.Bytes)
+	assert.NoError(err)
+	keyPemBlock, _ := pem.Decode(loadedKey)
+	_, err = x509.ParsePKCS1PrivateKey(keyPemBlock.Bytes)
+	assert.NoError(err)
+
+}
+
+func TestTLSServerStartup(t *testing.T) {
+	state.InitState()
 }
